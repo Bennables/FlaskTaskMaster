d0b69da8b05f8f78caed809fd8554ccce678a089
bennables <bensiriv@gmail.com>
2023-05-19 08:59:51 -0700

    creating the database

--- app.py
+++ app.py
@@ -1,6 +1,20 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABAS_URI'] = 'sqlite:///test.db'
+#database initialized with data from app
+db = SQLAlchemy(app)
+
+class Todo(db.Model):
+    #I assume this is the number for the task
+    id = db.Column(db.Integer, primary_key = True)
+    #this is the description for the task, 200chars max
+    content = db.Column(db.String(200), nullable = False)
+    completed = db.Column(db.Integer, default = 0)
+    date_created = db.Column(db.DateTime, default = datetime.utcnow)
+
 
 #settin gup rout
 @app.route('/')
